da58cefcbf97019fa02fafe8672bfad8ee1e9234
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-04-10 20:49:28 +0300

    login

--- signin/urls.py
+++ signin/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
     path('account/', views.account, name='account'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout')
+    path('logout/', views.user_logout, name='logout')
 ]
--- signin/views.py
+++ signin/views.py
@@ -1,9 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from pymongo import MongoClient
 
-# Create your views here.
 
-def login(request):
+def home(request):
+    context = {
+        'welcome_message': 'Welcome to my website!'
+    }
+    return render(request, 'home.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        # Check if passwords match
+        if password != confirm_password:
+            return render(request, 'register/register.html', {'error': 'Passwords do not match'})
+        
+        # Check if username or email already exists
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            return render(request, 'register/register.html', {'error': 'Username or email already exists'})
+        
+        # Save user to MongoDB database
+        client = MongoClient('mongodb+srv://eco:Titus1@3@cluster0.je0rm.mongodb.net/?retryWrites=true&w=majority')
+        db = client['toshcode']
+        users = db['users']
+        user_data = {'username': username, 'email': email, 'password': password}
+        result = users.insert_one(user_data)
+        print('User added with ID:', result.inserted_id)
+        
+        # Redirect to login page
+        return redirect('login')
+    
+    return render(request, 'register/register.html')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Connect to your MongoDB instance using PyMongo
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['your_database_name']
+        users_collection = db['your_users_collection_name']
+
+        # Look up the user by username and password hash in your MongoDB database
+        user_data = users_collection.find_one({'username': username, 'password_hash': hash(password)})
+        
+        if user_data:
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home') # Replace 'home' with the name of your desired home view
+
     return render(request, 'login/login.html')
 
+#login_required
 def account(request):
-    return render(request, 'account/account.html')
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'account/account.html', context)
+
+def user_logout(request):
+    logout=request.user
+    context = {
+        'logout': logout,
+    }    
+    return redirect(request,'home', context)
